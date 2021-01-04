3705efcd176142ae17b8f48ac8a641636732adfd
nickolay.petrash <nickolay_petrash@epam.com>
2021-01-04 14:04:25 +0300

    setup analog triger for movment control

--- device/esp32/rc-car/rc-car.ino
+++ device/esp32/rc-car/rc-car.ino
@@ -2,63 +2,97 @@
 // https://dl.espressif.com/dl/package_esp32_index.json
 // Upload speed 115200
 
-
 #include <PS4Controller.h>
+#include <ESP32Servo.h>
 
 // PIN
-const int pin_fwd = 32;
-const int pin_bwd = 33;
-const int pin_speed = 34;
-const int pin_servo = 35;
+const int pin_fwd = 27;
+const int pin_bwd = 26;
+const int pin_speed = 14;
+const int pin_servo = 33;
+
+// l289n
+const int freq = 30000;
+const int pwmChannel = 0;
+const int resolution = 8;
+
+const int dutyCycleMin=140;
+const int dutyCycleMax=250;
 
 
 void setup()
 {
     Serial.begin(115200);
-    // Connect to dualshock 4, using predifined mac with SixaxisPairTool
-    PS4.begin("03:03:03:03:03:03");
+
+    // connect to dualshock 4, using predifined mac with SixaxisPairTool
+    if(!PS4.begin("03:03:03:03:03:03")){
+        Serial.println("Could not begin PS4");
+    }
+    // attach controller callback
+    PS4.attach(handleControllerEvent);
+
+    // setup pins
+    pinMode(pin_fwd, OUTPUT);
+    digitalWrite(pin_fwd, LOW);
+    
+    pinMode(pin_bwd, OUTPUT);
+    digitalWrite(pin_bwd, LOW);
+
+    pinMode(pin_speed, OUTPUT);
+    digitalWrite(pin_speed, LOW);
+
+    ledcSetup(pwmChannel, freq, resolution);
+    ledcAttachPin(pin_speed, pwmChannel);
 }
 
-void controller() {
+void handleControllerEvent() {
     // move forward - R2
     if (PS4.data.button.r2)
     {
-        Serial.print("r2 button at ");
-        Serial.println(PS4.data.analog.button.r2, DEC);
+        uint8_t r2 = PS4.data.analog.button.r2;
+        Serial.println(r2);
+
+        int dutyCycle = map(r2, 15, 255, dutyCycleMin, dutyCycleMax);
+
+        digitalWrite(pin_fwd, HIGH);
+        digitalWrite(pin_bwd, LOW);
+
+        ledcWrite(pwmChannel, dutyCycle);
     }
 
-    // move backward -  L2
-    if (PS4.data.button.l2) 
+     // move backward -  L2
+    if (PS4.data.button.l2)
     {
-        Serial.print("l2 button at ");
-		Serial.println(PS4.data.analog.button.l2, DEC);
-	}
+        uint8_t l2 = PS4.data.analog.button.l2;
+        Serial.println(l2);
 
-    // controll direction - L3
-    if (PS4.event.analog_move.stick.lx)
-    {
-        Serial.print("Left Stick x at ");
-        Serial.println(PS4.data.analog.stick.lx, DEC);
+        int dutyCycle = map(l2, 15, 255, dutyCycleMin, dutyCycleMax);
+
+        digitalWrite(pin_bwd, HIGH);
+        digitalWrite(pin_fwd, LOW);
+
+        ledcWrite(pwmChannel, dutyCycle);
     }
 
-    if (PS4.event.analog_move.stick.ly)
-    {
-        Serial.print("Left Stick y at ");
-        Serial.println(PS4.data.analog.stick.ly, DEC);
-    }
+    // direction - L3
+    // if (PS4.event.analog_move.stick.lx)
+    // {
+    //     Serial.print("Left Stick x at ");
+    //     int8_t x = PS4.data.analog.stick.lx;
+    //     servo.write(map(PS4.data.analog.stick.lx, 0, 255, 0, 180));
+    // }
 }
+
+
+
 
 void loop()
 {
-    if(!PS4.isConnected()) {
-        Serial.println("Could not connect to Dualshock4");
-        return;
-    }
- 
-    // Print controller baterry status
-    Serial.print("Battey Percent : ");
-    Serial.println(PS4.data.status.battery, DEC);
-
-    // start listen controller events
-    PS4.attach(controller);
+    // test motor speed control
+    // digitalWrite(pin_fwd, HIGH);
+    // for(int dutyCycle = 0; dutyCycle <= 1000; dutyCycle++) {
+    //     ledcWrite(pwmChannel, dutyCycle);
+    //     delay(500);
+    //     Serial.println(dutyCycle);
+    // }
 }
